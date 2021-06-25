efe78a300dcf6f3e6b275d34c2bf8d29c5fe1eb1
marknown <marknown@qq.com>
2021-06-25 13:50:18 +0800

    V1.1.0 添加命令行执行函数

---
+++ command.go
@@ -0,0 +1,35 @@
+package utils
+
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
+	"runtime"
+)
+
+// Exec 执行执行的命令
+func Exec(args ...string) (string, error) {
+	cmd := exec.Command(args[0], args[1:]...)
+	var output bytes.Buffer
+	cmd.Stdout = &output
+	err := cmd.Run()
+	if nil != err {
+		return "", err
+	}
+
+	return output.String(), nil
+}
+
+// ExecInOS 在不同的环境中执行命令
+func ExecInOS(cmd string) (string, error) {
+	var args = []string{}
+	if runtime.GOOS == "windows" {
+		args = append(args, "cmd", "/c")
+	} else {
+		args = append(args, "/bin/sh", "-c")
+	}
+
+	args = append(args, cmd)
+	fmt.Println(args)
+	return Exec(args...)
+}
