bf9ad904e73c55838ce27eb25321c8335fa2ccd6
marknown <marknown@qq.com>
2021-06-28 11:45:39 +0800

    V1.1.3 添加异步命令行执行函数

--- command.go
+++ command.go
@@ -4,19 +4,33 @@ import (
 	"bytes"
 	"os/exec"
 	"runtime"
+	"strings"
 )
 
-// Exec 执行执行的命令
-func Exec(args ...string) (string, error) {
+// Exec Run 执行执行的命令（阻塞直到完成）
+func ExecRun(args ...string) (string, error) {
 	cmd := exec.Command(args[0], args[1:]...)
 	var output bytes.Buffer
 	cmd.Stdout = &output
 	err := cmd.Run()
+
 	if nil != err {
 		return "", err
 	}
 
 	return output.String(), nil
+}
+
+// Exec Start 执行执行的命令（不会等待该命令完成即返回）
+func ExecStart(args ...string) (string, error) {
+	cmd := exec.Command(args[0], args[1:]...)
+	err := cmd.Start()
+
+	if nil != err {
+		return "", err
+	}
+
+	return "异步执行无输出", nil
 }
 
 // ExecInOS 在不同的环境中执行命令
@@ -28,6 +42,13 @@ func ExecInOS(cmd string) (string, error) {
 		args = append(args, "/bin/sh", "-c")
 	}
 
+	cmd = strings.TrimSpace(cmd)
 	args = append(args, cmd)
-	return Exec(args...)
+
+	// 如果命令以 & 调用异步执行命令
+	if strings.HasSuffix(cmd, "&") {
+		return ExecStart(args...)
+	}
+
+	return ExecRun(args...)
 }
